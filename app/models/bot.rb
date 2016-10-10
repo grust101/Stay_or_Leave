e503ec07121844a25b82e16dfd99a3f97e506944
Georgia Rust <georgia.rust@gmail.com>
2016-10-10 13:05:38 -0700

    Create search words class method

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,2 +1,9 @@
 class Bot < ApplicationRecord
+	def self.search_words words
+		@responses = []
+		CLIENT.search(words, lang: "en").take(100).each do |response|
+			 @responses << response.text 
+			end
+		@responses	
+	end
 end
