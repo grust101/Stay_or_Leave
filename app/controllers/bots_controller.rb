da01dab523b271767cf35e4d5eb216c6d59a92ef
Georgia Rust <georgia.rust@gmail.com>
2016-10-10 16:02:46 -0700

    Write match links and remove links methods

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -2,7 +2,7 @@ class BotsController < ApplicationController
 	include BotsHelper
 	def home
 		stayed_tweets = Bot.search_words("#WhyIStayed").uniq
-		@unique_stayed_responses = remove_retweets(stayed_tweets).slice(0,10)
+		p @unique_stayed_responses = remove_retweets(stayed_tweets).slice(0,10)
 		left_tweets = Bot.search_words("#WhyILeft")
 		@unique_left_responses = remove_retweets(left_tweets).slice(0,10)
 
--- app/helpers/bots_helper.rb
+++ app/helpers/bots_helper.rb
@@ -4,4 +4,13 @@ module BotsHelper
 			tweet.split[0] != "RT"
 		end
 	end
+
+	def remove_link tweet
+		link = match_link(tweet)
+		tweet.gsub(/https:\/\/t.co\/[a-zA-Z0-9].+?(?=\W|$){8,10}/, "")
+	end
+
+	def match_link tweet
+		link = tweet.match(/https:\/\/t.co\/[a-zA-Z0-9].+?(?=\W|$){8,10}/).to_s
+	end
 end
